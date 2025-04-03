411f8150b00b582940bc55cb06d2e145cac2053a
Alexander Vinokourov <alexanderv2023@list.ru>
2025-04-04 01:49:11 +0300

    build 0.4.3
    1. Теперь пишется, кому вы выдали!
    2. Добавлено логирование в консоль/
    3. Улучшена кастомизация

--- java/ru/velfan/NineEyes.java
+++ java/ru/velfan/NineEyes.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import java.io.File;
-
 import java.util.ResourceBundle;
 
 public final class NineEyes extends JavaPlugin implements CommandExecutor {
@@ -17,55 +16,90 @@ public final class NineEyes extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         saveDefaultConfig();
         String langCode = getConfig().getString("language", "en_US");
         File langDir = new File(getDataFolder(), "lang");
-        if (!langDir.exists()) {
-            langDir.mkdirs();
-        }
+        if (!langDir.exists()) langDir.mkdirs();
+
         saveResource("lang/en_US.properties", true);
         saveResource("lang/ru_RU.properties", true);
         lang = ResourceBundle.getBundle("lang/" + langCode);
+
         getCommand("nightvision").setExecutor(this);
+
+        getLogger().info("NineEyes plugin enabled!"); // Стартовая запись в лог
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("NineEyes plugin disabled!"); // Финализационная запись в лог
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!cmd.getName().equalsIgnoreCase("nightvision")) {
-            return false;
-        }
+        try {
+            if (!cmd.getName().equalsIgnoreCase("nightvision")) return false;
 
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(lang.getString("command_only_for_players"));
-            return true;
-        }
-
-        Player player = (Player) sender;
-
-        if (!player.hasPermission("nineeyes.nightvision")) {
-            player.sendMessage(lang.getString("no_permission"));
-            return true;
-        }
-
-        if (args.length > 0) {
-            Player targetPlayer = getServer().getPlayer(args[0]);
-            if (targetPlayer == null) {
-                sender.sendMessage(lang.getString("player_not_found"));
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(lang.getString("command_only_for_players"));
                 return true;
             }
-            player = targetPlayer;
-        }
 
-        if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
-            player.removePotionEffect(PotionEffectType.NIGHT_VISION);
-            player.sendMessage(lang.getString("night_vision_disabled"));
-        } else {
-            int nightVisionDuration = getConfig().getInt("night_vision_duration", 6000);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, nightVisionDuration, 1));
-            player.sendMessage(lang.getString("night_vision_enabled"));
-        }
+            Player player = (Player) sender;
 
-        return true;
+            if (!player.hasPermission("nineeyes.nightvision")) {
+                player.sendMessage(lang.getString("no_permission"));
+                return true;
+            }
+
+            Player target = player;
+            if (args.length > 0) {
+                target = getServer().getPlayer(args[0]);
+                if (target == null) {
+                    sender.sendMessage(lang.getString("player_not_found"));
+                    return true;
+                }
+            }
+
+            boolean isSelf = target.equals(player);
+            String effectAction;
+
+            if (target.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
+                target.removePotionEffect(PotionEffectType.NIGHT_VISION);
+                effectAction = "disabled";
+
+                if (isSelf) {
+                    player.sendMessage(lang.getString("night_vision_disabled"));
+                } else {
+                    player.sendMessage(lang.getString("night_vision_disabled_other").replace("%player%", target.getName()));
+                }
+            } else {
+                int duration = getConfig().getInt("night_vision_duration", 6000);
+                target.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, duration, 1));
+                effectAction = "enabled";
+
+                if (isSelf) {
+                    player.sendMessage(lang.getString("night_vision_enabled"));
+                } else {
+                    player.sendMessage(lang.getString("night_vision_enabled_other").replace("%player%", target.getName()));
+                }
+            }
+
+            // Логирование действия
+            String logMessage = String.format(
+                    "[NightVision] %s %s night vision for %s",
+                    player.getName(),
+                    effectAction,
+                    isSelf ? "themselves" : target.getName()
+            );
+            getLogger().info(logMessage);
+
+            return true;
+
+        } catch (Exception e) {
+            getLogger().severe("Error executing command: " + e.getMessage()); // Логирование ошибок
+            e.printStackTrace();
+            return false;
+        }
     }
 }
